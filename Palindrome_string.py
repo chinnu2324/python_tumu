edc11bbc791f97419f56580219df39b3f612a5af
gangatumu <48506536+chinnu2324@users.noreply.github.com>
2024-07-08 18:16:42 -0500

    Update Palindrome_string.py

--- Palindrome_string.py
+++ Palindrome_string.py
@@ -1,3 +1,19 @@
+# Algorithm for a palindrome
+
+def ispalindrome(str):
+    startindex = 0
+    endindex = len(str) - 1
+    
+    for x in str:
+        print(str[startindex])
+        print(str[endindex])
+        if str[startindex] != str[endindex]:
+            return False
+        startindex += 1 
+        endindex -= 1    
+    return True  
+print(ispalindrome('bomb'))
+#  second
 x = input('Enter the word:')
 y= x[::-1]
 if x == y:
